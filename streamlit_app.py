527b3ef74e643ddf14742ed7e95851df29f39528
Muhammed Muzammil K M <120074499+Muhammed-km@users.noreply.github.com>
2024-07-08 21:04:09 +0530

    Update streamlit_app.py
    api changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize your smoothie :cup_with_straw:")
@@ -46,3 +47,8 @@ if ingredients_list:
 
         # Display success message
         st.success('Your Smoothie is ordered for {}! 🥤'.format(name_on_order))
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
