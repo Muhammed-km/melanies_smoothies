6d3729e2234ac5e91ce33a09b2fe77df70645a2f
Muhammed Muzammil K M <120074499+Muhammed-km@users.noreply.github.com>
2024-07-08 22:00:53 +0530

    Update streamlit_app.py
    adding all fruits nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,9 @@ if ingredients_list:
     # Remove trailing space
     ingredients_string = ingredients_string.strip()
     for fruit_chosen in ingredients_list:
-	    ingredients_string += fruit_chosen + ' ' 
-	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	    ingredients_string += fruit_chosen + ' '
+	    st.subheader(fruit_chosen + ' Nutrition Information')
+	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
 	    fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     # Button to submit the order
