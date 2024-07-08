6e119061b56bae4963428371c99ab6d217de85c0
Muhammed Muzammil K M <120074499+Muhammed-km@users.noreply.github.com>
2024-07-08 22:19:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ st.stop()
 
 #converting the snowpark dataframe to a pandas 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 # Multiselect for choosing ingredients with max selection limit
 ingredients_list = st.multiselect(
@@ -41,6 +41,8 @@ if ingredients_list:
     ingredients_string = ingredients_string.strip()
     for fruit_chosen in ingredients_list:
 	    ingredients_string += fruit_chosen + ' '
+	    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+	    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 	    st.subheader(fruit_chosen + ' Nutrition Information')
 	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
 	    fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
